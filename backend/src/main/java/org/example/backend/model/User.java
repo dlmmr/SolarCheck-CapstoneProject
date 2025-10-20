061c801ad1964bd0775f810d67d7e8044a8d3341
Dennis Lemmer <dlemmer28@gmail.com>
2025-10-20 17:23:28 +0200

    fixed testing, added UUID to User

--- backend/src/main/java/org/example/backend/model/User.java
+++ backend/src/main/java/org/example/backend/model/User.java
@@ -1,9 +1,13 @@
 package org.example.backend.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public record User(
+
+        @Id
+        String userId,
         UserInfo userInfo,
         UserConditions userConditions,
         UserResult userResult
